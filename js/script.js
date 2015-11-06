6b3cb9ef5495861a1542e6ee4632f268fc02b208
José Becerra <antonio.becerra@grupomedios.com>
2015-11-06 13:39:57 -0600

    gym

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+ 
+  $("#owl-example").owlCarousel({
+  	  navigation : false, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true
+  });
+});
