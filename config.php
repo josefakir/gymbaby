982168f09ef17d7c3930af51818d82f8098d0914
José Becerra <antonio.becerra@grupomedios.com>
2015-11-09 11:27:41 -0600

    gymbaby

--- config.php
+++ config.php
@@ -1,3 +1,6 @@
 <?php 
-
+	$host = "127.0.0.1";
+	$user = "josefaki_gymbaby";
+	$pass = "enj163XK.";
+	$db = "josefaki_gymbaby";
 ?>
--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 					<li><a href="https://www.facebook.com/estimulaciontemprana.adomicilio/about" target="_blank"><img src="images/facebook.png" alt="Facebook"></a></li>
 					<li><a href="https://twitter.com/gymbabyathome" target="_blank"><img src="images/twitter.png" alt="Twitter"></a></li>
 				</ul>
-				<a href="/" id="logo"><img src="images/logo.png" alt="Gym Baby" class="w100"></a>
+				<a href="" id="logo"><img src="images/logo.png" alt="Gym Baby" class="w100"></a>
 				<ul id="menu">
 					<li><a href="index.php" class="inicio emenu">Inicio</a></li>
 					<li><a href="quienes-somos.php" class="quienes emenu">¿Quiénes Somos?</a></li>
